3092b693b47f7b6186652a2395e3f3fad55039c9
ashutosh-k-root <ashutosh.dev.root@gmail.com>
2026-01-19 22:20:02 +0530

    Feat: Font VictorMono

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,9 +24,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Victor Mono",
+        body: "Victor Mono",
+        code: "Victor Mono",
       },
       colors: {
         // default: {
